2d5156e143729b5027a61249e8620829c56e3b43
oderele stanley <oderele@kali>
2025-04-03 07:40:42 -0400

    VARIABLE AND STRING

---
+++ class-demo/hello.py
@@ -0,0 +1,25 @@
+# This line is running python code without a variable
+print("Hello word")
+print("I am learning python")
+
+
+# working with a variable
+first_name = "Oderele"
+last_name = "Stanley"
+
+# concatenation(concatenating a variable)
+print(first_name + " " + last_name)
+
+# Types of variables
+# Integer whole number (eg 0-9
+# # floating-point (float) - Decimal numbers (e.g 0)
+#string (str) - Text Data ("hello", "python",)
+# Baloonean (bool) - True?False values
+
+age = 18
+sentence = "The age to vote in"
+country = "Tanzania"
+
+full_sentence = sentence + "" + country
+
+print(full_sentence + " is " + str(age))
